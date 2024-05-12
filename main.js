1b733f00df7b771818043a815a910211aeacda92
Etcetera Zahz <157307429+GenoyVI@users.noreply.github.com>
2024-05-11 21:38:52 -0500

    Added JS Validations To Form

--- main.js
+++ main.js
@@ -12,3 +12,11 @@ const formEls = [
   document.querySelector("#message"),
 ];
 console.log(formEls);
+
+const submitBtn = document.querySelector("button[type='submit']");
+
+formEls.forEach((el) => {
+  el.addEventListener("blur", (e) => {
+    console.log("blur event", e.target.id);
+  });
+});
